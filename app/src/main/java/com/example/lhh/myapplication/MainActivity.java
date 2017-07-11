7ca58fe4d472490a8c37ffdb6fbdde706ed6c697
weiyufeng <6461114@qq.com>
2017-07-11 13:52:29 +0800

    打 了一个log

--- app/src/main/java/com/example/lhh/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lhh/myapplication/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.lhh.myapplication;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("","");
     }
 }
